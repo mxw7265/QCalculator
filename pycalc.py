366b7c16ef900c91a17adc4185e437f4ba7b8982
Micah Wood <micahwood50@gmail.com>
2020-09-27 13:54:13 -0400

    Added Header Documentations

--- pycalc.py
+++ pycalc.py
@@ -1,5 +1,12 @@
+#!/usr/bin/env python3
+
+# Filename: pycalc.py
+
+"""PyCalc is a simple calculator built using Python and PyQt5."""
+
 import sys
 
+# Import QApplication and the required widgets from PyQt5.QtWidgets
 from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget
 
 def main():
