3c629ec7828b6355f37650c3f36b8468b1cfcad0
Micah Wood <micahwood50@gmail.com>
2020-09-27 14:00:01 -0400

    Set up View model and Client code

--- pycalc.py
+++ pycalc.py
@@ -9,8 +9,25 @@ import sys
 # Import QApplication and the required widgets from PyQt5.QtWidgets
 from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget
 
+class PyCalcUi(QMainWindow):
+    """PyCalc's View (GUI)"""
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle('PyCalc')
+        self.setFixedSize(235, 235)
+
+        self._centralWidget = QWidget(self)
+        self.setCentralWidget(self._centralWidget)
+
+# Client code
 def main():
-    pass
+    pycalc = QApplication(sys.argv);
+
+    view = PyCalcUi()
+    view.show()
+
+    sys.exit(pycalc.exec_())
 
 if __name__ == "__main__":
     main()
